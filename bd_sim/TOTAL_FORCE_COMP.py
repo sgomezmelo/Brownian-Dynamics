1b8636f690961a097fd67ec919e7d003058c41c9
Santiago Gomez <santiagogomez@GomezMelo-S-wifi1.tphys.uni-heidelberg.de>
2022-04-19 18:44:18 +0200

    Commented and updated scripts

--- bd_sim/TOTAL_FORCE_COMP.py
+++ bd_sim/TOTAL_FORCE_COMP.py
@@ -1,74 +1,63 @@
 from funcs import *
 from forces import *
 
-
 def total_force_comp(x, y, alpha, p1x, p1y, p2x, p2y, a1x, a1y, a2x, a2y, params):
 
     '''Step 2 Calculate distances between particles, patches and the angles between patch alignment vectors'''
-    # distance, absolute distance
-    dist_matrix_x = compute_dist(x, params)
-    dist_matrix_y = compute_dist(y, params)
-    absolute_dist_matrix = abs_dist_matrix(dist_matrix_x, dist_matrix_y)
-   # print('min distance in force',absolute_dist_matrix.min())
-   # print(np.unravel_index(np.argmin(absolute_dist_matrix),absolute_dist_matrix.shape ))
+    # Calculate dX and dY differences between particle bodies
+    dist_matrix_x = compute_dist(x, params) # Matrix of Dx between COMs Xi-Xj
+    dist_matrix_y = compute_dist(y, params) # Matrix of Dy distances between COMs Yi-Yj
+    absolute_dist_matrix = abs_dist_matrix(dist_matrix_x, dist_matrix_y) #Distances between COMs
 
-    # patch distance, angle 12
-    patch_dist_matrix_12_x = compute_dist_patch_AB(p1x, p2x, params)
-    patch_dist_matrix_12_y = compute_dist_patch_AB(p1y, p2y, params)
+
+    #Patch distance, angle 12
+    patch_dist_matrix_12_x = compute_dist_patch_AB(p1x, p2x, params) # Matrix Xipatch1 - Xjpatch2
+    patch_dist_matrix_12_y = compute_dist_patch_AB(p1y, p2y, params) # Matrix Xipatch1 - Xjpatch2
     abs_dist_12 = abs_dist_matrix(patch_dist_matrix_12_x, patch_dist_matrix_12_y)
     theta_12 = compute_angle_patch_AB(a1x, a1y, a2x, a2y)
-
-    # patch distance, angle 21
-    patch_dist_matrix_21_x = - patch_dist_matrix_12_x.T
-    patch_dist_matrix_21_y =  - patch_dist_matrix_12_y.T
+    
+    #Distances and angles are reflexive
     abs_dist_21 =  abs_dist_12.T
     theta_21 = theta_12.T
 
 
-
     '''Step 3 Calculate repulsive force matrices'''
+    #Calculates NxN matrices with pairwise forces along x and y
     f_rep_matrix_x = np.array(rep_force(dist_matrix_x, absolute_dist_matrix, params))
     f_rep_matrix_y = np.array(rep_force(dist_matrix_y, absolute_dist_matrix, params))
 
-    # calculate total repulsive force (array)
+    # Calculate total repulsive force by summing over one of the axes
     total_f_rep_x = total_force(f_rep_matrix_x)
     total_f_rep_y = total_force(f_rep_matrix_y)
 
-    # turn repulsive force off
-    # total_f_rep_x = np.array([0.0]*PARTICLE_NUMBER)
-    # total_f_rep_y = np.array([0.0]*PARTICLE_NUMBER)
 
     '''Step 4 Calculate attractive force matrices'''
-
+    #Compute forces between patch 1 of particle i and patch 2 of particle j
     f_att_matrix_x_12 = np.array(attr_force_AB(patch_dist_matrix_12_x, theta_12, abs_dist_12, params))
-    f_att_matrix_x_21 = - f_att_matrix_x_12.T
-
+    f_att_matrix_x_21 = - f_att_matrix_x_12.T #Newtons third law - Forces are equal in magnitude and opposite direction
+    
+    
     f_att_matrix_y_12 = np.array(attr_force_AB(patch_dist_matrix_12_y, theta_12, abs_dist_12, params))
-    f_att_matrix_y_21 = - f_att_matrix_y_12.T
+    f_att_matrix_y_21 = - f_att_matrix_y_12.T #Newtons third law - Forces are equal in magnitude and opposite direction
 
-    # needed to calculate the torque that act on patch A and B
-    total_f_att_matrix_x_patch1 = f_att_matrix_x_12
-    total_f_att_matrix_x_patch2 = f_att_matrix_x_21
 
-    total_f_att_matrix_y_patch1 = f_att_matrix_y_12
-    total_f_att_matrix_y_patch2 = f_att_matrix_y_21
-
-    # calculate total repulsive force on each patch (array)
-    total_f_att_x_patch1 = total_force(total_f_att_matrix_x_patch1)
-    total_f_att_x_patch2 = total_force(total_f_att_matrix_x_patch2)
-    total_f_att_y_patch1 = total_force(total_f_att_matrix_y_patch1)
-    total_f_att_y_patch2 = total_force(total_f_att_matrix_y_patch2)
+    # Total sum by summing over one of the axes
+    total_f_att_x_patch1 = total_force(f_att_matrix_x_12)
+    total_f_att_x_patch2 = total_force(f_att_matrix_x_21)
+    total_f_att_y_patch1 = total_force(f_att_matrix_y_12)
+    total_f_att_y_patch2 = total_force(f_att_matrix_y_21)
 
     '''Step 5 Calculate Total Force'''
     total_force_x = total_f_rep_x + total_f_att_x_patch1 + total_f_att_x_patch2
     total_force_y = total_f_rep_y + total_f_att_y_patch1 + total_f_att_y_patch2
 
 
-    #calculate orientational torque
+    #Orientational torque
     orientational_torque_matrix_21 = np.array(orientational_torque_AB(theta_21, abs_dist_21, a2x, a2y, a1x, a1y, params))
     orientational_torque_matrix_12 = np.array(orientational_torque_AB(theta_12, abs_dist_12, a1x, a1y, a2x, a2y, params))
-
+    #Sum torques from each patch
     total_orientational_torque_matrix = orientational_torque_matrix_12 + orientational_torque_matrix_21
+    #Summ all torques
     total_orientational_torque = total_force(total_orientational_torque_matrix)
 
 
@@ -85,45 +74,49 @@ def total_force_comp(x, y, alpha, p1x, p1y, p2x, p2y, a1x, a1y, a2x, a2y, params
             absolute_dist_matrix,
             total_orientational_torque)
 
+#@profile
 def total_force_comp_with_tail(x, y, alpha, p1x, p1y, p2x, p2y, a1x, a1y, a2x, a2y, sspx, sspy, params):
 
     '''Step 2 Calculate distances between particles, patches and the angles between patch alignment vectors'''
-    # distance, absolute distance
-    dist_matrix_x = compute_dist(x, params)
-    dist_matrix_y = compute_dist(y, params)
-    absolute_dist_matrix = abs_dist_matrix(dist_matrix_x, dist_matrix_y)
+    # COM distances
+    # Matrix Xi-Xj, Xi is the x coordinate of i-th com (antisymmetric)
+    dist_matrix_x = compute_dist(x, params) 
+    # Matrix Yi-Yj, Yi is the y coordinate of i-th com (antisymmetric)
+    dist_matrix_y = compute_dist(y, params) 
+    #Distances between coms (symmetric)
+    absolute_dist_matrix = abs_dist_matrix(dist_matrix_x, dist_matrix_y) 
 
-    # distance for the tails
-    dist_matrix_tail_x = compute_dist(sspx, params)
-    dist_matrix_tail_y = compute_dist(sspy, params)
-    absolute_dist_matrix_tail = abs_dist_matrix(dist_matrix_tail_x, dist_matrix_tail_y)
+    # Tail distances
+    # Matrix Xti-Xtj, Xti is the x coordinate of i-th tail (antisymmetric)
+    dist_matrix_tail_x = compute_dist(sspx, params) 
+    # Matrix Yti-Ytj, Yti is the y coordinate of i-th tail (antisymmetric)
+    dist_matrix_tail_y = compute_dist(sspy, params) 
+    #Distances among tails (symmetric)
+    absolute_dist_matrix_tail = abs_dist_matrix(dist_matrix_tail_x, dist_matrix_tail_y) 
 
-    # distance tail com
-    dist_matrix_tail_com_x = compute_dist_tail_com(sspx, x, params)
+    # COM - Tail distances
+    
+    # Matrix Xti-Xj, Xti is the x coordinate of i-th com  and Xj is the j-th tail x coordinate
+    dist_matrix_tail_com_x = compute_dist_tail_com(sspx, x, params) 
+    # Matrix Yti-Yj, Xti is the x coordinate of i-th com  and Xj is the j-th tail x coordinate
     dist_matrix_tail_com_y = compute_dist_tail_com(sspy, y, params)
+    # Matrix of distances between com and tail
     abs_dist_tail_com = abs_dist_matrix(dist_matrix_tail_com_x, dist_matrix_tail_com_y)
 
-    # distance com tail
-    dist_matrix_com_tail_x = -dist_matrix_tail_com_x.T
-    dist_matrix_com_tail_y =-dist_matrix_tail_com_y.T
-    abs_dist_com_tail = abs_dist_tail_com.T
 
-
-    # patch distance, angle 12
+    # Distances and angles between patches
+    # Xipatch1-Xjpatch2
     patch_dist_matrix_12_x = compute_dist_patch_AB(p1x, p2x, params)
+    # Yipatch1-Yjpatch2
     patch_dist_matrix_12_y = compute_dist_patch_AB(p1y, p2y, params)
+    #Distance between patches
     abs_dist_12 = abs_dist_matrix(patch_dist_matrix_12_x, patch_dist_matrix_12_y)
+    #Angle between patch1i and  patch2j
     theta_12 = compute_angle_patch_AB(a1x, a1y, a2x, a2y)
-
-    # patch distance, angle 21
-    patch_dist_matrix_21_x = - patch_dist_matrix_12_x.T
-    patch_dist_matrix_21_y =  - patch_dist_matrix_12_y.T
+    
+    #Distances and angles 21 should be the transpose of the 12
     abs_dist_21 =  abs_dist_12.T
     theta_21 = theta_12.T
-     #patch_dist_matrix_21_x = compute_dist_patch_AB(p2x, p1x, params)
-     #patch_dist_matrix_21_y = compute_dist_patch_AB(p2y, p1y, params)
-     #abs_dist_21 = abs_dist_matrix(patch_dist_matrix_21_x, patch_dist_matrix_21_y)
-     #theta_21 = compute_angle_patch_AB(a2x, a2y, a1x, a1y)
 
     '''Step 3 Calculate repulsive force matrices'''
     f_rep_matrix_x = np.array(rep_force(dist_matrix_x, absolute_dist_matrix, params))
@@ -134,41 +127,34 @@ def total_force_comp_with_tail(x, y, alpha, p1x, p1y, p2x, p2y, a1x, a1y, a2x, a
 
     f_rep_matrix_tail_com_x = np.array(rep_force(dist_matrix_tail_com_x, abs_dist_tail_com, params, tailscom=True))
     f_rep_matrix_tail_com_y = np.array(rep_force(dist_matrix_tail_com_y, abs_dist_tail_com, params, tailscom=True))
-
+    
+    #Repulsive forces COM-Tail should be negative of force Tail COM transposed
     f_rep_matrix_com_tail_x = -f_rep_matrix_tail_com_x.T
     f_rep_matrix_com_tail_y = -f_rep_matrix_tail_com_y.T
-
     force_on_tail_matrix_x = f_rep_matrix_tail_x + f_rep_matrix_tail_com_x
     force_on_tail_matrix_y = f_rep_matrix_tail_y + f_rep_matrix_tail_com_y
 
-    # calculate total repulsive force (array)
+    # Calculate total repulsive force (array)
     total_f_rep_x = total_force(f_rep_matrix_x + f_rep_matrix_com_tail_x + force_on_tail_matrix_x)
     total_f_rep_y = total_force(f_rep_matrix_y + f_rep_matrix_com_tail_y + force_on_tail_matrix_y)
-
-    # turn repulsive force off
-    # total_f_rep_x = np.array([0.0]*PARTICLE_NUMBER)
-    # total_f_rep_y = np.array([0.0]*PARTICLE_NUMBER)
 
     '''Step 4 Calculate attractive force matrices'''
 
     f_att_matrix_x_12 = np.array(attr_force_AB(patch_dist_matrix_12_x, theta_12, abs_dist_12, params))
     f_att_matrix_x_21 =  - f_att_matrix_x_12.T
-
     f_att_matrix_y_12 = np.array(attr_force_AB(patch_dist_matrix_12_y, theta_12, abs_dist_12, params))
     f_att_matrix_y_21 =  - f_att_matrix_y_12.T
 
-    # needed to calculate the torque that act on patch A and B
-    total_f_att_matrix_x_patch1 = f_att_matrix_x_12
-    total_f_att_matrix_x_patch2 = f_att_matrix_x_21
-
-    total_f_att_matrix_y_patch1 = f_att_matrix_y_12
-    total_f_att_matrix_y_patch2 = f_att_matrix_y_21
 
     # calculate total repulsive force on each patch (array)
-    total_f_att_x_patch1 = total_force(total_f_att_matrix_x_patch1)
-    total_f_att_x_patch2 = total_force(total_f_att_matrix_x_patch2)
-    total_f_att_y_patch1 = total_force(total_f_att_matrix_y_patch1)
-    total_f_att_y_patch2 = total_force(total_f_att_matrix_y_patch2)
+    total_f_att_x_patch1 = total_force(f_att_matrix_x_12)
+    #print('patch1 x', total_f_att_x_patch1)
+    total_f_att_x_patch2 = total_force(f_att_matrix_x_21)
+    #print('patch2 x', total_f_att_x_patch2)
+    total_f_att_y_patch1 = total_force(f_att_matrix_y_12)
+    #print('patch1 y', total_f_att_y_patch1)
+    total_f_att_y_patch2 = total_force(f_att_matrix_y_21)
+    #print('patch2 y', total_f_att_y_patch2)
 
     total_f_tail_x = total_force(force_on_tail_matrix_x)
     total_f_tail_y = total_force(force_on_tail_matrix_y)
@@ -177,10 +163,12 @@ def total_force_comp_with_tail(x, y, alpha, p1x, p1y, p2x, p2y, a1x, a1y, a2x, a
     total_force_x = total_f_rep_x + total_f_att_x_patch1 + total_f_att_x_patch2
     total_force_y = total_f_rep_y + total_f_att_y_patch1 + total_f_att_y_patch2
 
-    #calculate orientational torque
+    #Calculate orientational torque
     orientational_torque_matrix_12 = np.array(orientational_torque_AB(theta_12, abs_dist_12, a1x, a1y, a2x, a2y, params))
     orientational_torque_matrix_21 = np.array(orientational_torque_AB(theta_21, abs_dist_21, a2x, a2y, a1x, a1y, params))
     total_orientational_torque_matrix = orientational_torque_matrix_12 + orientational_torque_matrix_21
+    
+    #Sum all pairwise torques
     total_orientational_torque = total_force(total_orientational_torque_matrix)
 
     return (total_force_x,
@@ -200,8 +188,3 @@ def total_force_comp_with_tail(x, y, alpha, p1x, p1y, p2x, p2y, a1x, a1y, a2x, a
 
 
 
- #p1 = np.array([1,2,3])
- #p2 =  np.array([4,5,6])
- #params = {'boxlength' : 5}
- #print(compute_dist_patch_AB(p1, p2, params))
- #print(compute_dist_patch_AB(p2, p1, params))
