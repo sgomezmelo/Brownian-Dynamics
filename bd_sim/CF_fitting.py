da356390160e1425ca3d7d9d5e658d61d636278c
Santiago Gomez <santiagogomez@GomezMelo-S-wifi1.tphys.uni-heidelberg.de>
2022-04-21 16:25:24 +0200

    User friendlier CFfitting

--- bd_sim/CF_fitting.py
+++ bd_sim/CF_fitting.py
@@ -9,51 +9,44 @@ Created on Wed Nov  3 14:10:40 2021
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
-from scipy.integrate import solve_ivp, odeint
-import sys
-import proplot as pplt
-sys.path.append('/Users/santiagogomez/Downloads/Pplot')
-from pubproplot import *
+from scipy.integrate import solve_ivp
+import json
 
-#Initialize variables
-N = 10
-N_min_closed = 8
-N_closed = (N-N_min_closed)
-#folder = 'avg_micro_param/'
-folder = 'DataAnalysis_Cluster/average_multiple_parameter/'
-name = str(sys.argv[1])
-MC = int(sys.argv[2])
-e_att = float(sys.argv[3])
-time = np.loadtxt(folder+name+'_time'+'.csv', delimiter = ',')
-max_time = int(len(time))
-t = time[:max_time]
+#Extract names and arguments from txt file
+with open('args_CF_fit.txt','r') as f:
+    data = f.read()
+print(data)
+args = json.loads(data)
 
-#Extract relevant data of Concentrations and time
-N_particles = np.loadtxt(folder+name+'_average_total_number'+'.csv', delimiter = ',')
-N_particles = N_particles[:max_time]
-closedC = np.loadtxt(folder+name+'_average_closed'+'.csv', delimiter = ',')
-closedC = closedC[:max_time,N_min_closed:N]
-openC= np.loadtxt(folder+name+'_average_open'+'.csv', delimiter = ',')
-openC = openC[:max_time, 1:N+1]
-
-#Single array of concentrations
-C = np.concatenate((openC,closedC), axis = 1).T
-params = np.load(folder+'params.npz', allow_pickle=True)
+#Extract files from simulation and relevant parameters, based on txt file
+N = int(args['N max'])
+N_min_closed = int(args['N min'])
+name = str(args['Final File name'])
+folder = args['Results Folder']
+params = np.load(folder+args['Parameters'], allow_pickle=True)
 params = params['params'].item()
+MC = int(params['Montecarlo step'])
 dt = params['tstep']
+t = np.loadtxt(folder+args['Time file'], delimiter = ',')
+N_particles = np.loadtxt(folder+args['Total Number file'], delimiter = ',')
+closedC = np.loadtxt(folder+args['Closed concentrations file'], delimiter = ',')
+openC= np.loadtxt(folder+args['Open concentrations file'], delimiter = ',')
+
+#Extract relevant concentrations and parameters according to predefined Nmin and Nmax
+N_closed = (N-N_min_closed)
+closedC = closedC[:,N_min_closed:N]
+openC = openC[:, 1:N+1]
+C = np.concatenate((openC,closedC), axis = 1).T #Single array of concentrations
 initial_particles = params['nparticles']
 k_i = 1.0/(MC*dt)
-
 No_parameters_to_fit = 2*(N_closed)+2
 k0 = 7.0*np.ones(No_parameters_to_fit)
+r0 = np.ones(2) 
+r0[0] = 3.0
+
 print(k0)
 
-#First fit adsorption Kinetics using total particle number
-def FD(x,x0,alpha):
-    f = np.zeros(x.shape)
-    f[x <= x0+15.0/alpha] = 1.0/(1.0+np.exp(alpha*(x-x0)))
-    return f
-
+#Define adsorption kinetics as a second order polynomial, according to RSA kinetics in low limit
 def dNdt(t,N,rate_constants,k_i):
     L = params['boxlength']
     R1 = params['RBIG']
@@ -65,6 +58,7 @@ def dNdt(t,N,rate_constants,k_i):
     dN = k_i-k1*x+k2*x**2
     return dN
 
+#Define cost function for adsorption kinetics as the sum of squared errors
 def Adsorption_cost_function(x, N_obs, k_i, t,initial_particles):
     k = np.exp(x)
     y0 = [initial_particles]
@@ -72,32 +66,9 @@ def Adsorption_cost_function(x, N_obs, k_i, t,initial_particles):
     Estimated_N = sol.y
     weights = np.ones(len(N_obs))
     weights[N_obs>0.0] = 1/N_obs[N_obs>0.0]
-    #print('Current adsorption iter', x)
+    print('Current adsorption iter', x)
     res = np.sum((weights*(Estimated_N-N_obs))**2)/len(N_obs)
-    print('Error', res)
     return res
-    
-r0 = np.ones(2) 
-r0[0] = 3.0
-ads_result = minimize(Adsorption_cost_function, r0, args = (N_particles, k_i, t,initial_particles), method = 'Nelder-Mead')
-r_constants = np.exp(ads_result.x)
-print(r_constants)
-y0 = [initial_particles]
-N_solution = solve_ivp(dNdt, (t[0],t[-1]), y0, method = 'RK45' , args = (r_constants,k_i), t_eval = t)
-np.savetxt(folder+name+'_Adsorption_kinetics_solution.csv',N_solution.y,delimiter = ',')
-
-latexify()
-fig = pplt.figure(refaspect = 1.61)
-ax = fig.add_subplot(111)
-ax.scatter(t,N_particles, label = 'GCMC')
-ax.plot(t,(N_solution.y)[0,:], label = 'RSA')
-ax.set_xlabel(r'$\bar{t}$')
-
-ax.set_ylabel(r'$N_t$')
-format_axes(ax)
-ax.legend(loc = 'lower right', ncols = 1)
-plt.savefig('Adsorption_kinetics.pdf')
-print('Finished fitting adsorption parameters')
 
 #Calculates the derivative dCdt according to the simplified, discrete coagulation fragmentation equations
 # k - Rate constants vector, C - Concentration of open species, C_closed-concentration of rings, N_min_closed - lowest possible ring
@@ -120,10 +91,8 @@ def CF(t,Concentration, N_max, N_min_closed,k,k_ads,ads_constants,open_system = 
     
     for i in range(1,N_max):
         #Compute the terms of the CF equations except the closure and opening of rings
-        
         dCdt[i] = (k_on*np.sum(C[:i]*(C[:i][::-1])) - 2*k_on*C[i]*np.sum(C[:N_max-i-1])
         + 2*k_off*np.sum(C[i+1:])-k_off*i*C[i])
-        
         
         if i>= N_min_closed:
             #Account for opening and closure of rings
@@ -140,25 +109,7 @@ def CF(t,Concentration, N_max, N_min_closed,k,k_ads,ads_constants,open_system = 
     dCondt = np.concatenate((dCdt,dC_closed))
     return dCondt
 
-
-
-
-def Cost_finite_dif(x, C_obs_full, N_species, N_min, t_full, k_i, ads_params, time_step = 20 , include_monomers = True):
-    C_obs = C_obs_full[:,::time_step]
-    t = t_full[::time_step]
-    k = np.exp(-x)
-    dCobs = C_obs[:,2:]-C_obs[:,:-2]
-    dt = t[2:]-t[:-2]
-    #print(dt)
-    dCdt_obs = dCobs/(dt[0])
-    dC_CF = vectorized_CF(C_obs,N_species, N_min, k,k_i, ads_params)
-    weights = np.ones(dCdt_obs.shape)
-    #weights[dCdt_obs!=0.0] =  1/dCdt_obs[dCdt_obs!=0.0]
-    error = np.sum(((dCdt_obs-dC_CF[:,1:-1])/weights)**2)/C_obs.size
-    print(error)
-    print(x)
-    return error
-
+#Cost function for CF equations
 def CostFunction(x, C_obs, N_species, N_min, t, k_i, ads_params , initial_monomers = 1, include_monomers = True):
     
     X = np.exp(-x)
@@ -180,16 +131,22 @@ def CostFunction(x, C_obs, N_species, N_min, t, k_i, ads_params , initial_monome
     print('residuals', residuals)
     return residuals
 
-result = minimize(CostFunction, k0, args = (C, N, N_min_closed, t, k_i, r_constants), method = 'Nelder-Mead', options={'maxiter':2000})
-print(result)
+#First fit adsorption parameters
+ads_result = minimize(Adsorption_cost_function, r0, args = (N_particles, k_i, t,initial_particles), method = 'Nelder-Mead')
+r_constants = np.exp(ads_result.x)
+y0 = [initial_particles]
+N_solution = solve_ivp(dNdt, (t[0],t[-1]), y0, method = 'RK45' , args = (r_constants,k_i), t_eval = t)
+np.savetxt(folder+name+'_adsorption_kinetics_solution.csv',N_solution.y,delimiter = ',')
+print('Finished fitting adsorption parameters')
+
+result = minimize(CostFunction, k0, args = (C, N, N_min_closed, t, k_i, r_constants), method = 'Nelder-Mead', options={'maxiter':1000})
 k = np.exp(-result.x)
-print(k)
 y0 = np.zeros(N+N_closed)
 y0[0] = initial_particles
-solution = solve_ivp(CF, (0.0,time[-1]), y0, method = 'BDF' , args = (N, N_min_closed, k,k_i, r_constants), t_eval = time)
+solution = solve_ivp(CF, (0.0,t[-1]), y0, method = 'BDF' , args = (N, N_min_closed, k,k_i, r_constants), t_eval = t)
+print(solution)
 C_est = solution.y
 C_est_open = C_est[:N,:].T
-print(C_est_open.shape)
 C_est_closed = C_est[N:N+N_closed+1,:].T
 
 kinetic_constants = {
@@ -201,120 +158,9 @@ kinetic_constants = {
     'k_open':k[1:N_closed+1],
     'k_close':k[N_closed+1:2*N_closed+1]
     }
-print(kinetic_constants)
-#np.savetxt(folder+name+'_CF_open_solution.csv',C_est_open,delimiter = ',')
-#np.savetxt(folder+name+'_CF_closed_solution.csv',C_est_closed,delimiter = ',')
-#np.savez(folder+name+'CF_constants',kinetic_constants)
+np.savetxt(folder+name+'_open_solution.csv',C_est_open,delimiter = ',')
+np.savetxt(folder+name+'_closed_solution.csv',C_est_closed,delimiter = ',')
+np.savez(folder+name+'_rate_constants',kinetic_constants)
 
+print('Finished')
 
-fig = pplt.figure(refaspect = 1.61)
-pplt.rc.cycle = '538'
-ax = fig.add_subplot(111)
-format_axes(ax)
-
-for i in range(8):
-    #ax.plot(t,C_test[i,:])
-    color = next(ax._get_lines.prop_cycler)['color']
-    ax.plot(t,openC[:,i],color = color, label = str(i+1),alpha = 0.2)
-    ax.plot(t,C_est_open[:,i], color = color)
-ax.legend(loc = 'r', ncols = 1, frame = False)
-ax.set_ylabel('N')
-ax.set_xlabel(r'$\bar{t}$')
-plt.show()
-
-fig = pplt.figure(refaspect = 1.61)
-ax = fig.add_subplot(111)
-print(closedC[:,0])
-for i in range(N_closed):
-    color = next(ax._get_lines.prop_cycler)['color']
-    ax.plot(t,closedC[:,i],color = color, label = str(i+N_min_closed), alpha = 0.2)
-    ax.plot(t,C_est_closed[:,i], color = color)
-ax.set_ylabel('N')
-ax.set_xlabel(r'$\bar{t}$')
-ax.legend(loc = 'r', ncols = 1, frame = False)
-plt.show()
-#plt.plot(t,C_est[0,:])
-# plt.savefig('closed_CF.pdf')
-# plt.figure()
-# #plt.plot(t,(N_solution.y)[0,:]-np.sum(sizes[1:,np.newaxis]*C_est[1:,:], axis = 0), '--')
-# plt.plot(t,C[0,:], alpha = 0.2)
-# plt.plot(t,C_est[0,:])
-# plt.plot(t,C_est[1,:])
-# plt.plot(t,C[1,:], alpha = 0.5)
-# plt.plot(t,C_est[2,:])
-# plt.plot(t,C[2,:], alpha = 0.5)
-# plt.plot(t,C_est[3,:])
-# plt.plot(t,C[3,:], alpha = 0.5)
-# plt.plot(t,C_est[4,:])
-# plt.plot(t,C[4,:], alpha = 0.5)
-# plt.show()
-# def vectorized_CF(Concentration, N_max, N_min_closed,k,k_ads,ads_constants,open_system = True):
-    
-#     N_closed = N_max - N_min_closed
-#     #N_closed = 0
-#     # k_on = k[0]
-#     # k_off = k[1]
-#     # k_open = k[2:N_closed+2]
-#     # k_close = k[N_closed+2:]
-#     #k_on = k[0]
-#     k_off = 0.0
-#     k_on = k[0]
-#     k1 = k[1:]
-#     #k_off = k[1]
-#     #k_open = k[1:N_closed+1]
-#     #k_close = k[N_closed+1:]
-#     #Extract the number of species that can form ring
-    
-#     #Extract open and closed concentrations from an overall Concentration Matrix
-#     C = Concentration[:N_max,:]
-#     C_closed = Concentration[N_max:N_max+N_closed,:]
-
-#     #Prepare empty arrays with derivatives
-#     dCdt = np.zeros(C.shape)
-#     dC_closed = np.zeros(C_closed.shape)
-    
-#     #dCdt[0,:] =  (-2*C[0,:]*k_on*np.sum(C[:-1,:],axis = 0) + 2*k_off*np.sum(C[1:,:],axis = 0))
-#     dCdt[0,:] =  (-2*C[0,:]*np.sum(C[:-1,:]*k1[:-1,np.newaxis],axis = 0) + 2*k_off*np.sum(C[1:,:],axis = 0))
-#     #dCdt[-1] = (k_on*np.sum(C[:-1]*C[-2::-1]) - k_off*N_max*C[-1])
-    
-#     for i in range(1,N_max):
-#         #Compute the terms of the CF equations except the closure and opening of rings
-#         # delta = np.ones(len(C[:N-i-1]))
-#         # if len(C[:N-i-1])>i:
-#         #     delta[i] = 2.0
-            
-#         # delta2 = np.ones(len(C[i+1:]))
-#         # if len(delta2)>2*i:
-#         #     delta2[2*i] = 2.0
-            
-#         # if i%2 != 0:
-#         #     n_conf = i
-#         # else:
-#         #     n_conf = i-1
-        
-#         # dCdt[i] = (k_on*np.sum(C[:i]*C[i-1::-1]) - 2*k_on*C[i]*np.sum(delta*C[:N-i-1])
-#         # + 2*k_off*np.sum(delta2*C[i+1:])-k_off*n_conf*C[i])
-        
-#         dCdt[i,:] = (k_on*np.sum(C[1:i,:]*(C[:i-1,:][::-1,:]),axis = 0)+k1[i]*C[0,:]*C[i-1,:] - 2*k_on*C[i,:]*np.sum(C[1:N_max-i-1,:],axis = 0)
-#         - 2*k1[i]*C[i,:]*C[0,:] + 2*k_off*np.sum(C[i+1:,:],axis = 0)-k_off*i*C[i,:])
-        
-        
-#         if i>= N_min_closed:
-#             #Account for opening and closure of rings
-#             #print(i-N_min_closed)
-#             #dCdt[i,:] += - k_close[i-N_min_closed]*C[i,:] + k_open[i-N_min_closed]*C_closed[i-N_min_closed,:]
-#             #dC_closed[i-N_min_closed,:] = k_close[i-N_min_closed]*C[i,:] - k_open[i-N_min_closed]*C_closed[i-N_min_closed,:]
-#             dC_closed[i-N_min_closed] = 0
-#     #Add source term from the reservoir
-#     size = np.arange(N_max)+1.0
-#     #dCdt[0] = dNdt(t,N_tot,ads_constants,k_ads,params) - np.sum(size[1:]*dCdt[1:])- np.sum(dC_closed)
-#     if open_system == True:
-#         c_size = np.zeros(N_max+N_closed)
-#         c_size[:N_max] = np.arange(N_max+1.0)[1:]
-#         c_size[N_max:] = np.arange(N_closed)+N_min_closed
-#         N_tot = np.sum(c_size[:,np.newaxis]*Concentration[:N_max+N_closed,:],axis = 0)
-#         #dCdt[0] += k_ads*FD(N_tot,x0,a)*(1.0) + (1.0-FD(N_tot,x0,a))*k_des*(N_j-N_tot)**3
-#         dCdt[0,:] += dNdt(t,N_tot,ads_constants,k_ads)
-#     dCondt = np.concatenate((dCdt,dC_closed))
-#     return dCondt
-#
